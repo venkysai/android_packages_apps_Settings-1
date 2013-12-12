c1745b80a893b2553a7a61afe8d36f60d6d36f4b
Michael Bestas <mikeioannina@gmail.com>
2013-12-12 05:40:54 +0200

    Forward port CM Screen Security settings (2/2)
    Fingerprint: Forward challenge extras
    Settings: forward decrypt required on boot flag
    Settings: allow rotation while setting new pattern
    Settings: use the actual user id to set pattern size
    Settings: handle decrypting larger pattern sizes
    CryptKeeper: pattern unlock displays incorrect pw when correct
    Settings: fix non lock pattern CryptKeeper crash
    CryptKeeper: layout whole screen in bounds
    CryptKeeper improvements
    Settings: Use GLIF Theme for missing Settings > Screen lock Activities
    
    @mydongistiny:
    Updated for oreo and fixed force close
    Added switches for show dots and error path
    Added MultiLinePreference and GLIF Theme stuff
    Moved switches behind the gear icon to match stock
    Change-Id: I7078d703c218cd096d9b77c003a94b52fbce6322

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -112,6 +112,7 @@ import com.android.settings.notification.ZenModeSettings;
 import com.android.settings.notification.ZenModeVisualInterruptionSettings;
 import com.android.settings.password.ChooseLockPassword;
 import com.android.settings.password.ChooseLockPattern;
+import com.android.settings.password.ChooseLockPatternSize;
 import com.android.settings.print.PrintJobSettingsFragment;
 import com.android.settings.print.PrintSettingsFragment;
 import com.android.settings.security.LockscreenDashboardFragment;
@@ -260,6 +261,7 @@ public class SettingsGateway {
             HazardSettings.class.getName(),
             ThemesFragment.class.getName(),
             ButtonSettings.class.getName(),
+            ChooseLockPatternSize.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

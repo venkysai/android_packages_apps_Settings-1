08d765f09caf3c5f8382f347a51f6e6d82989c3e
Kshitij Gupta <kshitijgm@gmail.com>
2018-01-23 06:42:32 -0500

    Jump to homescreen and back on theme change
    *DU Edits*
    - Modified to bring the user back to the fragment they were in
    
    - Change string for the theme list preference. Is no longer just SystemUI
    
    @jhenrique09 edits:
    
    * Adapt to Settings app
    * Use FLAG_ACTIVITY_REORDER_TO_FRONT flag
    * Show back icon
    
    @xyyx: jump to our ThemesFragment

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -131,6 +131,7 @@ import com.android.settings.wifi.WifiAPITest;
 import com.android.settings.wifi.WifiInfo;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
+import com.android.settings.aoscp.colormanager.ThemesFragment;
 
 import com.toxyc.hazard.HazardSettings;
 
@@ -256,6 +257,7 @@ public class SettingsGateway {
             BluetoothDeviceDetailsFragment.class.getName(),
             DataUsageList.class.getName(),
             HazardSettings.class.getName(),
+            ThemesFragment.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
